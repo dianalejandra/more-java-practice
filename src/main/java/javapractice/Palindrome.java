8cec98be5d36f647403810ac7e8b10b34e06f4b7
dianalejandra <diana.abate@gmail.com>
2023-09-21 18:13:48 -0300

    Working on palindrome exercise

--- src/main/java/javapractice/Palindrome.java
+++ src/main/java/javapractice/Palindrome.java
@@ -1,4 +1,5 @@
 package javapractice;
+import java.util.Scanner;
 
 /*
 Dada una oracion, indicar si es un palindromo
@@ -8,6 +9,33 @@ Given a sentence, show if it is a palindrome
 public class Palindrome {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Insert your sentence to check if it is a palindrome or not: ");
+        String sentence = keyboard.nextLine();
+
+        String cleanSentence = sentence.replaceAll("[^a-zA-Z]","").toLowerCase();
+        boolean palindrome = isPalindrome(cleanSentence);
         
+        if(palindrome){
+            System.out.println("The sentence is a palindrome");
+        }else{
+            System.out.println("The sentence is not a palindrome");
+        }        
+    }
+    
+    public static boolean isPalindrome(String text){
+        int left = 0;
+        int right = text.length()-1;
+        
+        while(left < right){
+            if(text.charAt(left) != text.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
+
+
