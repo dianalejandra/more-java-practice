d1dfc1d2920e0381ca47f0bab2d7014cb5892fdd
dianalejandra <diana.abate@gmail.com>
2024-06-09 12:44:37 -0300

    Starting with new algorithm exercices

---
+++ src/main/java/AlgorithmExercises/ArraysIntegers.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AlgorithmExercises;
+
+/**
+ *
+ * @author diana
+ * 
+ * Dado un vector de enteros imprimir los numeros del vercor de izquierda a derecha y luego de derecha a izquierda
+ */
+public class ArraysIntegers {
+    
+}
---
+++ src/main/java/AlgorithmExercises/PrimeNumber.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javapractice;
+
+/**
+ *
+ * @author diana
+ * 
+ * Dado un númeo n, indicar si es primo o no, y en caso de que NO sea primo, indicar cuál es el divisor
+ * 
+ * 
+ */
+public class PrimeNumber {
+    public static void main(String[] args) {
+        
+    }
+    
+}
