63457e6510d912c50124359e8d9e9e3dc3754654
dianalejandra <diana.abate@gmail.com>
2024-06-09 14:19:32 -0300

    Started with integer array excercise. Need to figure out how to continue

--- src/main/java/AlgorithmExercises/ArraysIntegers.java
+++ src/main/java/AlgorithmExercises/ArraysIntegers.java
@@ -1,15 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AlgorithmExercises;
 
 /**
  *
  * @author diana
  * 
- * Dado un vector de enteros imprimir los numeros del vercor de izquierda a derecha y luego de derecha a izquierda
+ * Dado un vector de enteros imprimir los numeros del vector de izquierda a derecha y luego de derecha a izquierda
  */
 public class ArraysIntegers {
-    
+    public static void main(String[] args) {
+        // we define and initialize the array
+        int[] myArray = new int [] {5, 2, 66, 87, 9, 34, 40, 100, 12, 20};
+        //int i = myArray.length -1;
+        
+        for(int element : myArray){
+            System.out.println("El vector de izq a der: " + element);
+        }
+        /*
+        for(i; i > myArray.length -1; i-- ){
+            System.out.println("El vector de der a izq: " + myArray[i]);
+        }
+        */
+    }
 }
