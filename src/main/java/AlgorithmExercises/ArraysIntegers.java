dbc725f1dcd1fbc7af5720555a6254b635de606c
dianalejandra <diana.abate@gmail.com>
2024-06-12 19:22:54 -0300

    adding a solution on ArrayInteger problem

--- src/main/java/AlgorithmExercises/ArraysIntegers.java
+++ src/main/java/AlgorithmExercises/ArraysIntegers.java
@@ -8,17 +8,37 @@ package AlgorithmExercises;
  */
 public class ArraysIntegers {
     public static void main(String[] args) {
-        // we define and initialize the array
+        arrayOriginal();
+        arrayReverse();
+    }
+    
+    public static void arrayOriginal(){
+    // we define and initialize the array
         int[] myArray = new int [] {5, 2, 66, 87, 9, 34, 40, 100, 12, 20};
-        //int i = myArray.length -1;
+        int i;
+        
+        for (i=0; i < myArray.length; i ++){
+            System.out.println("El vector de izq a der: "+ myArray[i]);
+        } 
+        
+        /*
         
         for(int element : myArray){
             System.out.println("El vector de izq a der: " + element);
         }
-        /*
         for(i; i > myArray.length -1; i-- ){
             System.out.println("El vector de der a izq: " + myArray[i]);
         }
         */
+    }
+    
+    public static void arrayReverse(){
+        int[] myArray = new int [] {5, 2, 66, 87, 9, 34, 40, 100, 12, 20};
+        int i;
+        
+        for(i = myArray.length-1; i>=0; i--){
+            System.out.println("El vector de der a izq: " + myArray[i]);
+        }
+    
     }
 }
