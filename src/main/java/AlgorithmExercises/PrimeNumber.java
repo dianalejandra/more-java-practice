514c3836e99a621dde23d599fed7ba1780595b74
dianalejandra <diana.abate@gmail.com>
2024-06-15 21:00:31 -0300

    starting with prime numbers exercise

--- src/main/java/AlgorithmExercises/PrimeNumber.java
+++ src/main/java/AlgorithmExercises/PrimeNumber.java
@@ -5,12 +5,21 @@ package AlgorithmExercises;
  *
  * @author diana
  * 
- * Dado un númeo n, indicar si es primo o no, y en caso de que NO sea primo, indicar cuál es el divisor
+ * Dado un número n, indicar si es primo o no, y en caso de que NO sea primo, indicar cuál es el divisor
  * 
  * 
  */
 public class PrimeNumber {
     public static void main(String[] args) {
+        isPrime();
+    }
+    
+    public static void isPrime(){
+        int num = 4;
+        
+        if (num >1 && num / num == 1 || num / 1 == num){
+            System.out.println("El número " + num + " es primo");
+        }
         
     }
     
