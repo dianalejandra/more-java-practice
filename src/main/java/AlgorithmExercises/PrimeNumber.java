5c67f6ed29f6100a76bff41a9049bfe073197434
dianalejandra <diana.abate@gmail.com>
2024-06-27 17:56:22 -0300

    making user input a number and adding divisor logic

--- src/main/java/AlgorithmExercises/PrimeNumber.java
+++ src/main/java/AlgorithmExercises/PrimeNumber.java
@@ -1,4 +1,5 @@
 package AlgorithmExercises;
+import java.util.Scanner;
 
 /** Given a random number, indicate if it is prime or not. If it is not, show the possible divisors.*/
 public class PrimeNumber {
@@ -8,21 +9,26 @@ public class PrimeNumber {
     }
 
     public static void isPrime() {
-        int num = 2;       
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is prime: ");
+        int num = keyboard.nextInt();
+        int dividend = 0;
         boolean primeCheck = false;
         if(num <= 1){ // excluding 0 and 1
-            System.out.println(num + " NOT a prime number");
+            System.out.println(num + " is NOT a prime number");
         } else {
             for (int i = 2; i <= num / 2; i++) {
                 if (num % i == 0 ) {
                     primeCheck = true;
+                    dividend = i;
                     break;
                 }
             }
             if (!primeCheck) {
                 System.out.println(num + " IS a prime number");
             } else {
-                System.out.println(num + " NOT a prime number");
+                System.out.println(num + " is NOT a prime number");
+                System.out.println("Dividend: " + dividend);
             }
         }
     }
