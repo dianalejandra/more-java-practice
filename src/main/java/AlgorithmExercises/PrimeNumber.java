6728861c35a85a27fcc34fe81ad300e96d885ad7
Diana Abate <diana.abate@gmail.com>
2024-07-07 19:20:42 -0300

    adding logic for finding difisors for non prime numbers

--- src/main/java/AlgorithmExercises/PrimeNumber.java
+++ src/main/java/AlgorithmExercises/PrimeNumber.java
@@ -1,9 +1,13 @@
 package AlgorithmExercises;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/** Given a random number, indicate if it is prime or not. If it is not, show the possible dividends.*/
+/**
+ * Given a random number, indicate if it is prime or not. If it is not, show the
+ * possible divisors.
+ */
 public class PrimeNumber {
 
     public static void main(String[] args) {
@@ -14,31 +18,21 @@ public class PrimeNumber {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter a number to check if it is prime: ");
         int num = keyboard.nextInt();
-        int dividend = 0; // dividend can be stored in an array
-        int [] dividendArray = new int [] {};
-        List <Integer> dividendList = new ArrayList<>();
-        boolean primeCheck = false;
-        if(num <= 1){ // excluding 0 and 1
+        List<Integer> divisorList = new ArrayList<>(); // we create a list to store possible divisors 
+        if (num <= 1) { // excluding 0 and 1
             System.out.println(num + " is NOT a prime number");
-        } else {
+        } else { // we do num/2 because a number is not divisible by more than half its value
             for (int i = 2; i <= num / 2; i++) {
-                if (num % i == 0 ) {
-                    primeCheck = true;
-                    dividend = i;
-                    dividendArray = 
-                    for(int s : dividendArray){
-                    dividend = Integer.parseInt(s);
-                    dividendList.add(dividend);                    
-                    }
+                if (num % i == 0) {
+                    divisorList.add(i);
                 }
             }
-            if (!primeCheck) {
+            if (divisorList.isEmpty()) {
                 System.out.println(num + " IS a prime number");
             } else {
                 System.out.println(num + " is NOT a prime number");
-                System.out.println("Dividends: " + dividendList);
+                System.out.println("Divisors: " + divisorList);
             }
         }
     }
-
 }
