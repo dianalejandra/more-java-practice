bf38b0bec4c2836d4716bbf7231d8eceec6b5fb8
dianalejandra <diana.abate@gmail.com>
2024-07-17 15:17:36 -0300

    started new chess exercise

---
+++ src/main/java/AlgorithmExercises/Chess.java
@@ -0,0 +1,26 @@
+package AlgorithmExercises;
+
+/**
+ * Simulate knight's movement and show all posible movements depending on where the piece is located
+ */
+public class Chess {
+    int knightI = 2;
+    int knight = 1;
+    public static void main(String[] args) {
+        int [][] board = new int [8][8];
+        for(int i=0; i < board.length; i++){
+            for(int j=0; j < board[i].length; j++){
+                System.out.print(board[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void knightMovement(){
+    /*
+           mv1I
+    
+    */
+    
+    }
+}
